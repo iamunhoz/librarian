8f101cfed877b1737e5f8e7ab0d475ec870cf7fc
Ivã Munhoz <iamunhoz@gmail.com>
2020-09-01 09:53:00 -0300

    added 'synonym' parameter to 'lexicon' class in 'askoxford.py'

--- askoxford.py
+++ askoxford.py
@@ -13,11 +13,12 @@ class lexicon:
         app_key = secret.SECRET_KEY # talvez eu precise mudar em que momento isso é atribuido
         app_id = secret.SECRET_ID   # idem acima
         language = 'en-gb'          # talvez de pra trocar?
-        
+
         #ativa a api
         self.word_id = word_id
         url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + self.word_id.lower()
         self.jsonfile = './dumpedWords/' + self.word_id + '.json'
+        
         # checa se existe json local. Se existe, use este. Se não, faça um dump online
         if os.path.exists(self.jsonfile):
             with open (self.jsonfile) as f:
@@ -27,21 +28,22 @@ class lexicon:
             self.jsonstr = json.loads(r.text)
             with open (self.jsonfile, 'w') as f:
                 json.dump(self.jsonstr, f)
+        
+        #parameters
         self.pronunciation = self.jsonstr['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
         self.type = self.jsonstr['results'][0]['type']
         self.ipa = self.jsonstr['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['phoneticSpelling']
         self.grammarclass = ''
+        self.synonyms = []
         for i in self.jsonstr['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['synonyms']:
             self.synonyms.append(i['text'])
         self.definitions = []
         self.etymology = ''
         self.derivatives = []
     
-    def speak(self):
-        #deu certo. Futuramente implementar uma forma mais rapida #TODO        
-        os.system('mplayer ' + self.pronunciation)
+    # methods
+    def speak(self): 
+        os.system('mplayer ' + self.pronunciation) #deu certo. Futuramente implementar uma forma mais rapida #TODO
 
- #self.ipa = self.jsonstr['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['phoneticSpelling']
-        
         #for i in self.jsonstr['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['synonyms']:
         #    self.synonyms.append(i['text'])
